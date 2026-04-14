ef94863a02e94bb9005ffd64614b1c343b134d47
Luiz Isaque <luizisaqueuchuu@gmail.com>
2026-04-14 20:00:44 -0300

    feat: adicionado a API fetch dentro do escutador

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
+
+
 document.getElementById('cep').addEventListener('blur', (e) => {
-    
+    const inputValue = e.target.value
+    fetch(`https://viacep.com.br/ws/${inputValue}/json/`)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result)
+            document.getElementById('estado').value = result.estado
+        })
+        .catch(error => {
+
+        })
 })
