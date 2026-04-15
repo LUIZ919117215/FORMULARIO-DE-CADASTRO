92cc7b47c5395664cdc13ed075ef6d771548ac56
Luiz Isaque <luizisaqueuchuu@gmail.com>
2026-04-14 21:28:48 -0300

    feat: criado a função removerTravessão

--- scripts.js
+++ scripts.js
@@ -1,14 +1,20 @@
 const adicionarNoInput =  (idInput, resInput) => document.getElementById(idInput).value = resInput
+const removerTravessão = (cep) => cep.split("").filter(num => num !== '-').join("")
+const validarNumeroCEP
 
 
 document.getElementById('cep').addEventListener('blur', (e) => {
-    const inputValue = e.target.value
+    console.log(e.target.value)
+    const inputValue = removerTravessão(e.target.value)
+    console.log(inputValue);
+    
+
     fetch(`https://viacep.com.br/ws/${inputValue}/json/`)
         .then(response => response.json())
         .then(result => {
             console.log(result)
             adicionarNoInput('cidade', result.localidade)
-            adicionarNoInput('estado', result.estado)
+            adicionarNoInput('estado', result.estado)         
         })
         .catch(error => {
 
