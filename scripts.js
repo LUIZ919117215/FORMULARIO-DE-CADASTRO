3693acccd3f463ddc8686efdbc130055996f26d2
Luiz Isaque <luizisaqueuchuu@gmail.com>
2026-04-16 19:55:06 -0300

    feat: criado if para verificar a quantidade de número no CEP

--- scripts.js
+++ scripts.js
@@ -1,20 +1,18 @@
-const adicionarNoInput =  (idInput, resInput) => document.getElementById(idInput).value = resInput
+const adicionarNoInput = (idInput, resInput) => document.getElementById(idInput).value = resInput
 const removerTravessão = (cep) => cep.split("").filter(num => num !== '-').join("")
-const validarNumeroCEP
 
 
 document.getElementById('cep').addEventListener('blur', (e) => {
-    console.log(e.target.value)
     const inputValue = removerTravessão(e.target.value)
-    console.log(inputValue);
     
-
+    if (inputValue.length !== 8) return document.getElementById('avisos').innerText = "Um CEP deve ter apenas 8 números"
+    
     fetch(`https://viacep.com.br/ws/${inputValue}/json/`)
         .then(response => response.json())
         .then(result => {
             console.log(result)
             adicionarNoInput('cidade', result.localidade)
-            adicionarNoInput('estado', result.estado)         
+            adicionarNoInput('estado', result.estado)
         })
         .catch(error => {
 
