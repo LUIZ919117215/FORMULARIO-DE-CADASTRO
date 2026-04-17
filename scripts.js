4847a71184a9bad51c5aae8937d1af6f3a076627
Luiz Isaque <luizisaqueuchuu@gmail.com>
2026-04-16 21:32:36 -0300

    feat: criado a função verifLetraCep e a usando num if

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 const adicionarNoInput = (idInput, resInput) => document.getElementById(idInput).value = resInput
 const removerTravessão = (cep) => cep.split("").filter(num => num !== '-').join("")
-
+const verifLetraCep = (cep) => String(Number(cep)) === 'NaN'
 
 document.getElementById('cep').addEventListener('blur', (e) => {
     const inputValue = removerTravessão(e.target.value)
-    
+
     if (inputValue.length !== 8) return document.getElementById('avisos').innerText = "Um CEP deve ter apenas 8 números"
+    if (verifLetraCep(inputValue)) return document.getElementById('avisos').innerText = "Um CEP deve ter somentes números"
     
     fetch(`https://viacep.com.br/ws/${inputValue}/json/`)
         .then(response => response.json())
