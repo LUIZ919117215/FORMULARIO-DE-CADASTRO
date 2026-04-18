dc8a744893d95df2d3e47bb452f742ed184d734c
Luiz Isaque <luizisaqueuchuu@gmail.com>
2026-04-18 17:56:05 -0300

    feat: criado e emplementado o método exibirAviso

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+const exibirAviso = (id, informacao) => document.getElementById(id).innerText = informacao
 const adicionarNoInput = (idInput, resInput) => document.getElementById(idInput).value = resInput
 const adicionaMultInput = (id1, id2, id3, id4, id5, cep, bairro, cidade, estado, regiao) => {
     const ids = [id1, id2, id3, id4, id5] 
@@ -20,19 +21,18 @@ document.addEventListener('DOMContentLoaded', () => {
 document.getElementById('cep').addEventListener('blur', (e) => {
     const inputValue = removerTravessão(e.target.value)
 
-    if (inputValue.length !== 8) return document.getElementById('avisos').innerText = "Um CEP deve ter apenas 8 números"
-    if (verifLetraCep(inputValue)) return document.getElementById('avisos').innerText = "Um CEP deve ter somentes números"
+    if (inputValue.length !== 8) return exibirAviso('avisos', "Um CEP deve ter apenas 8 números.")
+    if (verifLetraCep(inputValue)) return exibirAviso('avisos', "Um CEP deve ter somentes números.")
     
     fetch(`https://viacep.com.br/ws/${inputValue}/json/`)
         .then(response => response.json())
         .then(result => {
-        adicionaMultInput('cep', 'bairro', 'cidade', 'estado', 'regiao', result.cep, result.bairro, result.localidade, result.estado, result.regiao)
-
+            adicionaMultInput('cep', 'bairro', 'cidade', 'estado', 'regiao', result.cep, result.bairro, result.localidade, result.estado, result.regiao)
             document.getElementById('btnSub').addEventListener('click', () => {
                 localStorage.setItem('cep', JSON.stringify(result))
             })
         })
         .catch(error => {
-            return document.getElementById('avisos').innerText = `Erro: ${error}`
+            return exibirAviso('avisos', `Erro: ${error}`)
         })
 })
